360f94709a36fc2b1fabff4053b25ff18356823e
AYUSH DHAR <ayushdh2@gmail.com>
2024-08-17 18:25:06 +0530

    search advance & user authentication done

--- adPyBlog/home/urls.py
+++ adPyBlog/home/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('contact/', views.contact, name="contact"),
     path('search/', views.search, name="search"),
+    path('login/', views.handelLogin, name="login"),
+    path('signup/', views.signup, name="signup"),
+    path("logout/", views.handelLogout, name="logout"),
     path('test/', views.test, name="contact"),
 ]
--- adPyBlog/home/views.py
+++ adPyBlog/home/views.py
@@ -1,8 +1,10 @@
-from turtle import title
-from django.shortcuts import render, HttpResponse
+
+from django.shortcuts import render, redirect
 from django.contrib import messages # type: ignore
 from home.models import Contact
 from blog.models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout, login 
 
 # Create your views here.
 
@@ -28,13 +30,18 @@ def contact(request):
 
 
 def test(request):
-    return render(request, "home/contactSubmit.html")
+    return render(request, "home/login.html")
 
 def search(request):
     q = request.GET.get("q")
     if q == None:
         return render(request, "error.html")
-    allposts = Post.objects.filter(title__icontains=q)
+    if len(q) > 78:
+        allposts =[]
+    else:
+        allpostsTitle = Post.objects.filter(title__icontains=q)
+        allpostsContent = Post.objects.filter(content__icontains=q)
+        allposts = allpostsTitle.union(allpostsContent)
 
     params = {
         "posts": allposts,
@@ -42,3 +49,80 @@ def search(request):
         "q":q
     }
     return render(request, "home/search.html", params)
+
+def handelLogin(request):
+    if request.user.username != "":
+        return redirect("/")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is None:
+            messages.error(request, "Incorrect username or password")
+            return render(request, "home/login.html")
+        
+        login(request, user)
+        return redirect("/")
+           
+    return render(request, "home/login.html")
+
+def signup(request):
+    if request.user.username != "":
+        return redirect("/")
+    if request.method == "POST":
+        frs = request.POST.get('frs')
+        lst = request.POST.get('lst')
+        email = request.POST.get("email")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        if len(frs) < 2:
+            messages.error(request, "First name is too short")
+            return render(request, "home/signup.html")
+        
+        if len(lst) < 2:
+            messages.error(request, "Last name is too short")
+            return render(request, "home/signup.html")
+        
+        if len(email) < 4:
+            messages.error(request, "Email is too short")
+            return render(request, "home/signup.html")
+        
+        if len(username) < 2:
+            messages.error(request, "Username is too short")
+            return render(request, "home/signup.html")
+        
+        specialChars = ["~", "`", "=", "+", ";", ":", "!", "#", "$", "@", "%", "^", "&", "*", "(", ")", "{","}","|", chr(92), "'", '"', ",","<",">",".","/","?"]
+        
+        for char in username:
+            if char in specialChars:
+                messages.error(request, "Username not contains any special cherecter excape '-' and '_'")
+                return render(request, "home/signup.html")
+        
+        if len(password) < 7:
+            messages.error(request, "Pasword is too short")
+            return render(request, "home/signup.html")
+        
+        existUsername = User.objects.filter(username=username)
+        existEmail = User.objects.filter(email=email)
+
+        if len(existUsername) != 0 :
+            messages.error(request, "Username is already taken")
+            return render(request, "home/signup.html")
+        
+        if len(existEmail) != 0:
+            messages.error(request, "Email is already taken")
+            return render(request, "home/signup.html")
+        
+        user = User.objects.create_user(username, email, password)
+        user.first_name = frs
+        user.last_name = lst
+        user.save()
+        return redirect("/login")
+        
+    return render(request, "home/signup.html")
+
+def handelLogout(request):
+    logout(request)
+    return redirect("/")
